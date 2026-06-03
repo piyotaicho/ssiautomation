bdd4c30880043bb66c5705a7e587c6bb50be7c7e
Yasuhiro Yamamoto <mohyama@mohnet.com>
2026-06-03 17:28:20 +0900

    Fixes

--- src/P外来患者用紙出力-処置室注射依頼票.ps1
+++ src/P外来患者用紙出力-処置室注射依頼票.ps1
@@ -1,7 +1,7 @@
-# A外来患者用紙出力-処置室注射依頼票.ps1 を起動する
+﻿# A外来患者用紙出力-処置室注射依頼票.ps1 を起動する
 
 # オーバーレイの起動
-$overlay = Start-Process powershell.exe -ArgumentList "-WindowStyle Hidden -PassThru -File $PSScriptRoot\Utility-Overlay.ps1 -Color '#55FF8000'"
+$overlay = Start-Process powershell.exe -PassThru -ArgumentList "-WindowStyle Hidden -PassThru -File $PSScriptRoot\Utility-Overlay.ps1 -Color '#55FF8000'"
 
 Start-Sleep -Milliseconds 500 # オーバーレイが起動するのを少し待つ
 
@@ -17,6 +17,6 @@ try {
 }
 
 # 正常終了を「緑」で3秒間だけ通知する
-$successPS = PowerShell.exe -WindowStyle Hidden -File $PSScriptRoot\Overlay.ps1 -Message '自動処理が正常に完了しました！' -Color '#4400FF00'
+$successPS = Start-Process powershell.exe -PassThru -ArgumentList "-WindowStyle Hidden -File $PSScriptRoot\Overlay.ps1 -Message '自動処理が正常に完了しました！' -Color '#4400FF00'"
 Start-Sleep -Seconds 3
-Stop-Process $successPS.Id -ErrorAction SilentlyContinue
+Stop-Process -Id $successPS.Id -ErrorAction SilentlyContinue
