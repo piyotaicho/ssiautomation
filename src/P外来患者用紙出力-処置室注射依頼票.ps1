173d2db117f995772eb9e2412e90997a858af40e
Yasuhiro Yamamoto <mohyama@mohnet.com>
2026-06-05 14:04:09 +0900

    フィルタリングをなしにする(全部出力)-noFilterオプションを追加

--- src/P外来患者用紙出力-処置室注射依頼票.ps1
+++ src/P外来患者用紙出力-処置室注射依頼票.ps1
@@ -1,22 +1,31 @@
 ﻿# A外来患者用紙出力-処置室注射依頼票.ps1 を起動する
+param(
+    [switch]$noFilter
+)
 
 # オーバーレイの起動
-$overlay = Start-Process powershell.exe -PassThru -ArgumentList "-WindowStyle Hidden -PassThru -File $PSScriptRoot\Utility-Overlay.ps1 -Color '#55FF8000'"
-
+$overlay = Start-Process powershell.exe -PassThru -ArgumentList "-ExecutionPolicy bypass -WindowStyle Hidden -File $PSScriptRoot\Utility-Overlay.ps1 -Color '#55FF8000'"
 Start-Sleep -Milliseconds 500 # オーバーレイが起動するのを少し待つ
 
 # 処理を開始
 try {
-    & $PSScriptRoot\A外来患者用紙出力-処置室注射依頼票.ps1
+    # 実際の処理スクリプトを起動
+    if ($noFilter) {
+        & $PSScriptRoot\A外来患者用紙出力-処置室注射依頼票.ps1 -noFilter
+    } else {
+        & $PSScriptRoot\A外来患者用紙出力-処置室注射依頼票.ps1 
+    }
+
+    # 正常終了を「緑」で3秒間だけ通知する
+    Stop-Process -Id $overlay.Id -ErrorAction SilentlyContinue
+    $successPS = Start-Process powershell.exe -PassThru -ArgumentList "-ExecutionPolicy bypass -WindowStyle Hidden -File $PSScriptRoot\Utility-Overlay.ps1 -Message '自動処理が正常に完了しました！' -Color '#4400FF00'"
+    Start-Sleep -Seconds 3
+    Stop-Process $successPS.Id -ErrorAction SilentlyContinue
 } catch {
     Stop-Process -Id $overlay.Id -ErrorAction SilentlyContinue
     Write-Error $_
 
+    # エラー表示はクリックをまつ
     & $PSScriptRoot\Utility-FullScreenDialog.ps1 -Message $_.Exception.Message -Title '処置室依頼票発行'
-    exit 1
 }
 
-# 正常終了を「緑」で3秒間だけ通知する
-$successPS = Start-Process powershell.exe -PassThru -ArgumentList "-WindowStyle Hidden -File $PSScriptRoot\Overlay.ps1 -Message '自動処理が正常に完了しました！' -Color '#4400FF00'"
-Start-Sleep -Seconds 3
-Stop-Process -Id $successPS.Id -ErrorAction SilentlyContinue
