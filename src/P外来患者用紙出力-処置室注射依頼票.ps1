da70a08df25b141317e8db95828b090339dd4954
Yasuhiro Yamamoto <mohyama@mohnet.com>
2026-06-18 09:56:15 +0900

    処理開始時にExcelを強制終了してメモリ空間を改善するように変更

--- src/P外来患者用紙出力-処置室注射依頼票.ps1
+++ src/P外来患者用紙出力-処置室注射依頼票.ps1
@@ -3,6 +3,18 @@ param(
     [switch]$noFilter
 )
 
+. .\Tools-UserInteraction.ps1
+# Excelをすべて閉じる
+$excelProcs = Get-Process -Name excel -ErrorAction SilentlyContinue
+if ($excelProcs.Count -gt 0) {
+    $result = Confirm-UserOkCancel '現在動作しているExcelをすべて強制終了します、よろしければOK、処理を中止するにはキャンセルを押してください'
+    if ($result) {
+        $excelProcs | Stop-Process -ErrorAction SilentlyContinue | Out-Null
+    } else {
+        exit 0
+    }
+}
+
 # オーバーレイの起動
 $overlay = Start-Process powershell.exe -PassThru -ArgumentList "-ExecutionPolicy bypass -WindowStyle Hidden -File $PSScriptRoot\Utility-Overlay.ps1 -Color '#55FF8000'"
 Start-Sleep -Milliseconds 500 # オーバーレイが起動するのを少し待つ
--- src/P外来患者用紙出力-検査台帳.ps1
+++ src/P外来患者用紙出力-検査台帳.ps1
@@ -1,22 +1,35 @@
 ﻿# A外来患者用紙出力-検査台帳.ps1 を起動する
+. .\Tools-UserInteraction.ps1
+
+# Excelをすべて閉じる
+$excelProcs = Get-Process -Name excel -ErrorAction SilentlyContinue
+if ($excelProcs.Count -gt 0) {
+    $result = Confirm-UserOkCancel '現在動作しているExcelをすべて強制終了します、よろしければOK、処理を中止するにはキャンセルを押してください'
+    if ($result) {
+        $excelProcs | Stop-Process -ErrorAction SilentlyContinue | Out-Null
+    } else {
+        exit 0
+    }
+}
 
 # オーバーレイの起動
-$overlay = Start-Process powershell.exe -PassThru -ArgumentList "-WindowStyle Hidden -PassThru -File $PSScriptRoot\Utility-Overlay.ps1 -Color '#55FF8000'"
-
+$overlay = Start-Process powershell.exe -PassThru -ArgumentList "-ExecutionPolicy bypass -WindowStyle Hidden -File $PSScriptRoot\Utility-Overlay.ps1 -Color '#55FF8000'"
 Start-Sleep -Milliseconds 500 # オーバーレイが起動するのを少し待つ
 
 # 処理を開始
 try {
+    # 実際の処理スクリプトを起動
     & $PSScriptRoot\A外来患者用紙出力-検査台帳.ps1
+
+    # 正常終了を「緑」で3秒間だけ通知する
+    Stop-Process -Id $overlay.Id -ErrorAction SilentlyContinue
+    $successPS = Start-Process powershell.exe -PassThru -ArgumentList "-ExecutionPolicy bypass -WindowStyle Hidden -File $PSScriptRoot\Utility-Overlay.ps1 -Message '自動処理が正常に完了しました！' -Color '#4400FF00'"
+    Start-Sleep -Seconds 3
+    Stop-Process $successPS.Id -ErrorAction SilentlyContinue
 } catch {
     Stop-Process -Id $overlay.Id -ErrorAction SilentlyContinue
     Write-Error $_
 
+    # エラー表示はクリックをまつ
     & $PSScriptRoot\Utility-FullScreenDialog.ps1 -Message $_.Exception.Message -Title '検査台帳発行'
-    exit 1
 }
-
-# 正常終了を「緑」で3秒間だけ通知する
-$successPS = Start-Process powershell.exe -PassThru -ArgumentList "-WindowStyle Hidden -File $PSScriptRoot\Overlay.ps1 -Message '自動処理が正常に完了しました！' -Color '#4400FF00'"
-Start-Sleep -Seconds 3
-Stop-Process -Id $successPS.Id -ErrorAction SilentlyContinue
