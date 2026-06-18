f5a1e44edd44209a0081bdff53604a5bd1eedc6a
Yasuhiro Yamamoto <mohyama@mohnet.com>
2026-06-18 10:00:14 +0900

    表示したダイアログボックスがtopmostになるようにした

--- src/Tools-UserInteraction.ps1
+++ src/Tools-UserInteraction.ps1
@@ -1,6 +1,7 @@
 ﻿# GUIベースでユーザーの入力を促すユーティリティ
-# Last update: 2026-05-27 p
+# Last update: 2026-06-13 p
 Add-Type -AssemblyName Microsoft.VisualBasic
+Add-Type -AssemblyName System.Windows.Forms
 
 # ユーザーから文字列の入力を受ける
 function Confirm-UserInput {
@@ -8,7 +9,10 @@ function Confirm-UserInput {
         [string]$text,
         [string]$title = 'ユーザー入力'
     )
-    [Microsoft.VisualBasic.Interaction]::InputBox($text, $title, '')
+    $form = New-Object System.Windows.Forms.Form
+    $form.TopMost = $true
+    [Microsoft.VisualBasic.Interaction]::InputBox($form, $text, $title, '')
+    $form.Dispose()
 }
 
 # YesNoの選択 Yesで$true
@@ -17,7 +21,10 @@ function Confirm-UserYesNo {
         [string]$text,
         [string]$title = 'ユーザー確認'
     )
-    $result = [System.Windows.Forms.MessageBox]::Show($text, $title, 'YesNo', 'Question')
+    $form = New-Object System.Windows.Forms.Form
+    $form.TopMost = $true
+    $result = [System.Windows.Forms.MessageBox]::Show($form, $text, $title, 'YesNo', 'Question')
+    $form.Dispose()
     return ($result -eq 'Yes')
 }
 
@@ -27,7 +34,10 @@ function Confirm-UserOkCancel {
         [string]$text,
         [string]$title = 'ユーザー確認'
     )
-    $result = [System.Windows.Forms.MessageBox]::Show($text, $title, 'OkCancel', 'Question')
+    $form = New-Object System.Windows.Forms.Form
+    $form.TopMost = $true
+    $result = [System.Windows.Forms.MessageBox]::Show($form, $text, $title, 'OkCancel', 'Question')
+    $form.Dispose()
     return ($result -eq 'Ok')
 }
 
@@ -37,7 +47,10 @@ function Invoke-InformationDialog {
         [string]$text,
         [string]$title = '情報'
     )
-    [System.Windows.Forms.MessageBox]::Show($text, $title, 'Ok', 'Information') | Out-Null
+    $form = New-Object System.Windows.Forms.Form
+    $form.TopMost = $true
+    [System.Windows.Forms.MessageBox]::Show($form, $text, $title, 'Ok', 'Information') | Out-Null
+    $form.Dispose()
 }
 
 # 注意ダイアログを表示
@@ -46,7 +59,10 @@ function Invoke-AlertDialog {
         [string]$text,
         [string]$title = '情報'
     )
-    [System.Windows.Forms.MessageBox]::Show($text, $title, 'Ok', 'Warning') | Out-Null
+    $form = New-Object System.Windows.Forms.Form
+    $form.TopMost = $true
+    [System.Windows.Forms.MessageBox]::Show($form, $text, $title, 'Ok', 'Warning') | Out-Null
+    $form.Dispose()
 }
 
 # エラーダイアログを表示
@@ -55,7 +71,48 @@ function Invoke-ErrorDialog {
         [string]$text,
         [string]$title = '情報'
     )
-    [System.Windows.Forms.MessageBox]::Show($text, $title, 'Ok', 'Error') | Out-Null
+    $form = New-Object System.Windows.Forms.Form
+    $form.TopMost = $true
+    [System.Windows.Forms.MessageBox]::Show($form, $text, $title, 'Ok', 'Error') | Out-Null
+    $form.Dispose()
+}
+
+# ファイル選択ダイアログを表示
+function Invoke-FileSelectDialog {
+    param(
+        [string]$title = 'ファイルを選択してください',
+        [string]$filter = 'すべてのファイル|*.*',
+        [string]$initialDirectory
+    )
+
+    # パラメーターの確認
+    $filters = $filter -split '|'
+    if (($filters.Count % 2) -ne 0) {
+        throw "指定されたフィルタ $filter が不正です"
+    }
+
+    if ($initialDirectory -and (-not (Test-Path -PathType Container -Path $initialDirectory))) {
+        throw "指定された開始フォルダ $initialDirectory が不正です"
+    }
+
+    $fileDialog = New-Object System.Windows.Forms.OpenFileDialog
+    $form = New-Object System.Windows.Forms.Form
+    $form.TopMost = $true
+
+    $fileDialog.Title = $title
+    $fileDialog.Filter = $filter
+    if ($initialDirectory) {
+        $fileDialog.InitialDirectory = $initialDirectory
+    }
+
+    $resultPath = $null
+    if ($fileDialog.ShowDialog($form) -eq [System.Windows.Forms.DialogResult]::OK) {
+        $resultPath = $fileDialog.FileName
+    }
+    $form.Dispose()
+    $fileDialog.Dispose()
+
+    return $resultPath
 }
 
 # デフォルトアクションはなし
