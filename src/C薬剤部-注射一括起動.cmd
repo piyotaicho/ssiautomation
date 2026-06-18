38ee10c571eaa7cb3b4e6f6e4c50348765a44ef4
Yasuhiro Yamamoto <mohyama@mohnet.com>
2026-06-18 09:54:57 +0900

    薬剤部用ランチャー起動cmd

---
+++ src/C薬剤部-注射一括起動.cmd
@@ -0,0 +1,4 @@
+powershell.exe -executionpolicy bypass -command "& { . Y:\•a‰@\ˆã‹Ç\Y•wl‰È\‚â‚Ü‚à‚Æ\‰ğÍ\Tools-UserInteraction.ps1; . Y:\•a‰@\ˆã‹Ç\Y•wl‰È\‚â‚Ü‚à‚Æ\‰ğÍ\Tools-CheckLogin.ps1; if (-not (Invoke-checkLogin) ) { Invoke-ErrorDialog 'ƒƒOƒCƒ“‚µ‚Ä‚­‚¾‚³‚¢'; exit 1 }}"
+if %ERRORLEVEL%==1 exit
+
+start powershell.exe -windowstyle hidden -executionpolicy bypass -File Y:\•a‰@\ˆã‹Ç\Y•wl‰È\‚â‚Ü‚à‚Æ\‰ğÍ\Utility-Luncher.ps1 -JsonPath Y:\•a‰@\ˆã‹Ç\Y•wl‰È\‚â‚Ü‚à‚Æ\‰ğÍ\Config-Luncher-–òÜ•”’Ë.json -Title "–òÜ•”í’“(’Ë)"
